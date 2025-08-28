f8a0b1213b9da3b9a732ec1907618fffdd29be7c
iba200 <ibrahimathiongane2006@gmail.com>
2025-08-28 14:03:12 +0000

    update

--- app/admin/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField("Nom d'utilisateur", validators=[DataRequired()])
-    password = PasswordField("Mot de passe", validators=[DataRequired()])
-    submit = SubmitField("Se connecter")
